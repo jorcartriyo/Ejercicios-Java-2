bb0af6a8e132d3635d50103084e24e38e74ce1eb
jorcartriyo <33526483+jorcartriyo@users.noreply.github.com>
2018-01-09 19:35:07 +0100

    Add files via upload

---
+++ CuadradosPerfectos/src/cuadradosperfectos/CuadradosPerfectos.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cuadradosperfectos;
+import java.util.*;
+
+public class CuadradosPerfectos {
+
+    public static void main(String[] args){
+        Scanner entrada=new Scanner(System.in);
+       int numero=1;
+       int resultado=0;       
+       System.out.println("Dime el primer número");
+       int numero1=entrada.nextInt();
+       System.out.println("Dime el segundo número");
+       int numero2=entrada.nextInt();
+       int auxiliar=0;
+       if (numero1>numero2){
+           auxiliar=numero1;
+           numero1=numero2;
+           numero2=auxiliar;           
+       }
+        while (numero1<=numero2) {
+            numero1++;            
+            resultado =numero*numero;           
+            System.out.println(resultado);
+            numero++;
+        }
+        
+    }
+    
+}
